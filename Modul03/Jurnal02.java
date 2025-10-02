ac34406e0d39923db94424cf77464b53f3865163
andreahmadf03-wq <andreahmadf03@gmail.com>
2025-10-02 18:07:00 +0700

    andrei f

--- Modul03/Jurnal02.java
+++ Modul03/Jurnal02.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner andre = new Scanner(System.in);
+         
+        int a = andre.nextInt();
+        int b = andre.nextInt();
+        int c = andre.nextInt();
+
+        double rataRata = (a + b + c)/3.0;
+
+        System.out.printf("%.2f", rataRata);
+        
+
     }
 }
