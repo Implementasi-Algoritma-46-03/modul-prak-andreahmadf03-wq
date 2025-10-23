26b284de6ff36526280166e7b5150c9d3f136393
andreahmadf03-wq <andreahmadf03@gmail.com>
2025-10-23 17:14:35 +0700

    Jurnal Modul 06

--- Modul06/Jurnal01.java
+++ Modul06/Jurnal01.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 public class Jurnal01 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+
+    public static String getSapaan(int waktu) {
+        if (waktu >= 6 && waktu <= 11) {
+            return "pagi";
+        } else if (waktu >= 12 && waktu <= 14) {
+            return "siang";
+        } else if (waktu >= 15 && waktu <= 17) {
+            return "sore";
+        } else {
+            return "malam";
+        }
+    }
+
+    public static void sapaMahasiswa(String nama, String sapaan) {
+        System.out.println("Halo, " + nama + ". Selamat " + sapaan + ".");
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String nama1 = input.nextLine();
+        String nama2 = input.nextLine();
+        int waktu = input.nextInt();
+
+        String sapaan = getSapaan(waktu);
+
+        sapaMahasiswa(nama1, sapaan);
+        sapaMahasiswa(nama2, sapaan);
+
+        input.close();
     }
 }
