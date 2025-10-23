c0172f76e29abfa6ef4f05a9781d1eeb87a277d7
andreahmadf03-wq <andreahmadf03@gmail.com>
2025-10-23 15:54:33 +0700

    TP 06 new

--- Modul11/TP02.java
+++ Modul11/TP02.java
@@ -1,23 +1,6 @@
-import java.util.Scanner;
-
 public class TP02 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        // Minta user mengetik satu kalimat
-        String kalimat = input.nextLine();
-        input.close();
-
-        // Hapus semua huruf vokal menggunakan method hapusVokal
-        String hasil = hapusVokal(kalimat);
-
-        // Tampilkan hasil setelah huruf vokal dihapus
-        System.out.println(hasil);
-    }
-
-    // Method untuk menghapus semua huruf vokal
-    private static String hapusVokal(String teks) {
-        // replaceAll() akan mengganti semua huruf vokal dengan string kosong ""
-        return teks.replaceAll("[AIUEOaiueo]", "");
+    public static void main(final String[] args) {
+        // Kerjakan soalnya di sini
     }
 }
