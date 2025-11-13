c382ccc1c29f1522679ed5e4835b6f3576de0b9d
andreahmadf03-wq <andreahmadf03@gmail.com>
2025-11-13 16:49:24 +0700

    Jurnal 09

--- Modul09/Jurnal03.java
+++ Modul09/Jurnal03.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt(); 
+        boolean prima = true;    
+
+        if (N <= 1) {
+            prima = false;
+        } else {
+            
+            for (int i = 2; i <= Math.sqrt(N); i++) {
+                if (N % i == 0) { 
+                    prima = false;
+                    break; 
+                }
+            }
+        }
+
+        if (prima) {
+            System.out.println("YA");
+        } else {
+            System.out.println("BUKAN");
+        }
     }
-}
+}   
