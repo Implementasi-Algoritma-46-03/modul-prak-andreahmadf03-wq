decaaae2171704742ff6ff31d781af1a966d6eb1
andreahmadf03-wq <andreahmadf03@gmail.com>
2025-11-20 17:07:52 +0700

    Jurnal 10

--- Modul10/Jurnal03.java
+++ Modul10/Jurnal03.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
 
 public class Jurnal03 {
-    public static void main(String[] args) {
+
+    public static void main(final String[] args) {
+        // Kerjakan soalnya di sini
         Scanner input = new Scanner(System.in);
+
+        // Input bilangan bulat N di mana 1 <= N <= 100
         int n = input.nextInt();
-        int counter = 0;
+
+        // Inisialisasi variabel pelacak angka
+        int bilanganSaatIni = 0;
         
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(counter % 10);
-                counter++;
+        // menentukan baris ke samping dan ke bawah
+        for (int i = 1; i <= n; i++) {
+            // Nested For untuk membuat kolom dan membuat isi kolom seperti di output soal
+            for (int j = 1; j <= n - (i - 1); j++) {
+                System.out.print(bilanganSaatIni + " ");
+                bilanganSaatIni = (bilanganSaatIni + 1) % 10;
             }
+
+            // Menampilkan hasil
             System.out.println();
         }
+
+        // Menutup Scanner
+        input.close();
     }
 }
