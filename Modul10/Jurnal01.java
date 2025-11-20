084175b6b39b2cee4f2c5f118c8e698089b26343
andreahmadf03-wq <andreahmadf03@gmail.com>
2025-11-20 16:54:06 +0700

    jurnal 10

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -1,6 +1,17 @@
 public class Jurnal01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+            //untuk mencari barisan genap
+            if (i % 2 == 0) {
+                System.out.print(" "); 
+            }
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i + " ");  
+            }
+
+            System.out.println(); 
+        }
     }
 }
--- Modul10/Jurnal02.java
+++ Modul10/Jurnal02.java
@@ -1,6 +1,18 @@
-public class Jurnal02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();  
+
+        for (int i = 1; i <= N; i++) {
+
+            for (int j = 1; j <= N; j++) {
+
+                System.out.print((i * j) + " ");
+            }
+            System.out.println(); 
+        }
     }
 }
