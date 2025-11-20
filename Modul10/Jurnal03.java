c9fb022db52b15b7ac9c65b24ee08072812a4267
andreahmadf03-wq <andreahmadf03@gmail.com>
2025-11-20 16:51:05 +0700

    jurnal

--- Modul10/Jurnal03.java
+++ Modul10/Jurnal03.java
@@ -1,6 +1,17 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int counter = 0;
+        
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter % 10);
+                counter++;
+            }
+            System.out.println();
+        }
     }
 }
